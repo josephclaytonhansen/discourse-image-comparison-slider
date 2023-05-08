d0bd59283cb3f3f22235200c6f9ab77afaa5bdd7
Joseph Hansen <45859835+josephclaytonhansen@users.noreply.github.com>
2023-05-08 12:56:04 -0500

    Update plugin.rb

--- plugin.rb
+++ plugin.rb
@@ -2,4 +2,4 @@
 # about: A interactive (draggable) image slider to hold Image A and Image B in the same space and transition between them
 # version: 0.0.1
 # authors: Joseph Hansen
-# url: https://github.com/yourusername/basic-plugin
+# url: https://github.com/josephclaytonhansen/discourse-image-comparison-slider
