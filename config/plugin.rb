cd26a07aaae60a55d12e44b8bde00f1a0ac1268d
Joseph Hansen <45859835+josephclaytonhansen@users.noreply.github.com>
2023-05-09 09:19:38 -0500

    change to component

--- config/plugin.rb
+++
@@ -1,5 +0,0 @@
-# name: image-comparison-slider
-# about: A interactive (draggable) image slider to hold Image A and Image B in the same space and transition between them
-# version: 0.0.1
-# authors: Joseph Hansen
-# url: https://github.com/yourusername/basic-plugin
